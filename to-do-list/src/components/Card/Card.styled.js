09214c118f18e99ad433225d1952ebcacd772182
CamiFranca <camillaartc@gmail.com>
2023-07-12 15:37:19 -0300

    start-layout

--- to-do-list/src/components/Card/Card.styled.js
+++ to-do-list/src/components/Card/Card.styled.js
@@ -1,5 +1,84 @@
 import styled from "styled-components"
 
 export const CardContainer = styled.div`
-background-color: yellow;
+    height:100%;
+    width:100%;
+    display:flex;
+
+   
+    .header{
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        padding:51px;
+        background-color:white;
+        height:10vh;
+        width:100%;
+
+    .left{
+    width:25%;
+    display:flex;
+    justify-content:flex-start;
+    align-items:center;
+    .btn_menu{
+        margin:27px 27px 27px 0px;
+    }
+    .menu{
+        height:33px;
+        width:33px;
+       
+    }
+    .logo{
+        height:33px;
+        width:33px;
+        margin-right:18px;
+    }
+  }
+  .rigth{
+    width:30%;
+    display:flex;
+    justify-content:flex-end;
+    align-items:center;
+
+    .update{
+        margin: 0px 27px 0px 27px;
+       
+        img{
+            height:33px;
+            width:33px; 
+        }
+    
+    }
+    .equals{
+        margin-right:33px;
+        img{
+        height:33px;
+        width:33px;
+        
+    }
+    }
+    .picture_user{
+        height:33px;
+        width:33px; 
+        margin-left: 33px;
+    }
+  }
+  button{
+        border: none;
+        /* border-style:outset; */
+   
+    }
+
+    input{
+       outline:none;
+       width:65%;
+       height:6vh;
+       padding-left:21px;
+       border:none;
+       border-radius: 9px;
+       background-color: lightgrey;
+       /* box-shadow:5px 5px 5px 5px ; */
+    }
+  
+}
 `
--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+
+const GlobalStyled = createGlobalStyle` 
+ body {
+    margin: 0;
+    padding: 0;
+    outline:0;
+    
+  }
+
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyled />
     <App />
   </React.StrictMode>
 );
